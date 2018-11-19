329834228d3e9815193150cc91839decb47d4676
Tomasz Pleśniak <toma1777@easv365.dk>
2018-11-19 21:58:46 +0100

    add owner

--- src/app/owners/owner-add-page/owner-add-page.component.ts
+++ src/app/owners/owner-add-page/owner-add-page.component.ts
@@ -3,7 +3,9 @@ import {OwnersService} from '../../shared/services/owners.service';
 import {Router} from '@angular/router';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Pet} from '../../shared/models/Pet';
-import {BsModalService} from 'ngx-bootstrap';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {PetsService} from '../../shared/services/pets.service';
+import {Owner} from '../../shared/models/Owner';
 
 @Component({
   selector: 'app-owner-add-page',
@@ -19,19 +21,47 @@ ownerForm = new FormGroup({
   PhoneNumber: new FormControl(''),
   Pet: new FormControl('')
 });
+  maxDate = new Date();
+  petForm = new FormGroup({
+    Name: new FormControl(''),
+    Type: new FormControl(''),
+    Birthdate: new FormControl(''),
+    SoldDate: new FormControl(''),
+    Color: new FormControl(''),
+    Price: new FormControl('')
+  });
+  pets: Pet[];
+  selectedPet: Pet;
+  newOwner: Owner;
   constructor(private ownerService: OwnersService,
-              private router: Router, private modalService: BsModalService) { }
+              private router: Router, private modalService: BsModalService, private petService: PetsService) { }
 
   ngOnInit() {
+    this.refresh();
   }
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 addNewOwner() {
-const newOwner = this.ownerForm.value;
-this.ownerService.addNewOwner(newOwner).subscribe(() => {
+/*const newOwner = this.ownerForm.value;*/
+this.newOwner = this.ownerForm.value;
+this.newOwner.pet = this.selectedPet;
+this.ownerService.addNewOwner(this.newOwner).subscribe(() => {
   this.router.navigateByUrl('/owners');
 });
-
-}
+  }
+  createPet() {
+    const newPet = this.petForm.value;
+    this.petService.addNewPet(newPet).subscribe(() => {
+      this.refresh();
+    });
+  }
+  refresh() {
+    this.petService.getPets().subscribe( petList => {
+      this.pets = petList;
+    });
+  }
+  setPet(selectedPet: Pet) {
+    this.selectedPet = selectedPet;
+  }
 }
